c717f81b9791d91b51714c1faf3bd07d02c25463
Anmol Shah <rockerfame@gmail.com>
2017-03-24 15:55:28 -0300

    console log server js

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.use(function(req, res, next) {
   if ( req.user ) {
     console.log('Current User:', req.user.firstName, req.user.lastName);
   } else {
-    console.log('LOL??');
+    console.log('LOLWA??');
   }
   next();
 });
